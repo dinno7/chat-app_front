83ca2d1d7917d3137db34f9f2a12a28a9b523f75
Dinno <tahadlrb7@gmail.com>
2024-05-13 20:48:18 +0330

    feat(components): user avatar componenet

---
+++ src/components/UserAvatar.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useUser } from '@/composables/useUser'
+
+const { isAuth, currentUser, profilePicture } = useUser()
+</script>
+
+<template>
+  <div
+    v-if="isAuth"
+    class="overflow-hidden rounded-full w-9 aspect-ratio-square bg-brand-base ring-2 ring-brand-action"
+    :title="currentUser?.name"
+  >
+    <img
+      :src="profilePicture"
+      :alt="currentUser?.name + ' profile image'"
+      class="object-cover w-full h-full object-c"
+    />
+  </div>
+</template>
+
+<style scoped></style>
