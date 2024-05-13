f5fc4ab2737ff98e8b8b537b129b0050f50cedff
Dinno <tahadlrb7@gmail.com>
2024-05-13 13:44:31 +0330

    feat(auth): customize useFetch hook and add useUser composable

---
+++ src/composables/$useFetch.ts
@@ -0,0 +1,70 @@
+import { createFetch } from '@vueuse/core'
+import { useUser } from './useUser'
+
+let requestOptions: RequestInit = {}
+const $useFetch = createFetch({
+  baseUrl: `${import.meta.env.VITE_SERVER_URL}/api`,
+  combination: 'chain',
+
+  options: {
+    timeout: 10000,
+    updateDataOnError: true,
+    beforeFetch(ctx) {
+      ctx.options
+      const accessToken = localStorage.getItem('accessToken') || ''
+      if (accessToken) {
+        ctx.options.headers = {
+          ...ctx.options.headers,
+          Authorization: `Bearer ${accessToken}`
+        }
+      }
+
+      requestOptions = ctx.options
+      return ctx
+    },
+    async onFetchError(ctx) {
+      ctx.error = ctx.data
+      const { setTokens, clearTokens } = useUser()
+      const storeRefreshToken = localStorage.getItem('refreshToken') || ''
+      const storeAccessToken = localStorage.getItem('accessToken') || ''
+      let isRefresh = false
+      if (ctx.response?.status === 401 && storeRefreshToken && !isRefresh) {
+        isRefresh = true
+
+        const getTokens = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/auth/refresh`, {
+          method: 'POST',
+          body: JSON.stringify({
+            refreshToken: storeRefreshToken
+          }),
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${storeAccessToken}`
+          }
+        })
+
+        const newTokens = await getTokens.json()
+
+        if (newTokens?.ok) {
+          setTokens(newTokens.data)
+
+          if (ctx.response?.url) {
+            await fetch(ctx.response.url, {
+              ...requestOptions,
+              headers: {
+                Authorization: `Bearer ${newTokens?.data?.accessToken || ''}`
+              }
+            })
+          }
+        } else {
+          clearTokens()
+        }
+      }
+
+      isRefresh = false
+
+      return ctx
+    }
+  }
+})
+
+export default $useFetch
---
+++ src/composables/useUser.ts
@@ -0,0 +1,99 @@
+import { getUserFromApi } from '@/api/user'
+import {
+  type BasePayload,
+  type SignAuthPayload,
+  type SignInData,
+  type SignUpData,
+  type User
+} from '@/types'
+import { createGlobalState, tryOnBeforeMount } from '@vueuse/core'
+import { computed, ref } from 'vue'
+import $useFetch from './$useFetch'
+
+export const useUser = createGlobalState(() => {
+  const currentUser = ref<User | null>(null)
+  const isAuth = computed<boolean>(() => !!currentUser.value?.email)
+  const fetching = ref(false)
+
+  tryOnBeforeMount(fetchUser)
+  function setUser(newUser: User | null) {
+    currentUser.value = newUser
+  }
+
+  async function signIn(signInData: SignInData) {
+    fetching.value = true
+    const { data, error } = await $useFetch('auth/signin')
+      .post(signInData)
+      .json<BasePayload<SignAuthPayload>>()
+    fetching.value = false
+    if (error.value || !data.value?.ok)
+      throw new Error(error.value?.message || 'Some problem happend in signing in')
+
+    const { data: tokens } = data.value
+    setTokens(tokens)
+
+    await fetchUser()
+
+    return currentUser
+  }
+
+  async function signUp(signUpData: SignUpData) {
+    fetching.value = true
+    const { data, error } = await $useFetch('auth/signup')
+      .post(signUpData)
+      .json<BasePayload<SignAuthPayload>>()
+    fetching.value = false
+    if (error.value || !data.value?.ok)
+      throw new Error(error.value?.message || 'Some problem happend in signing up')
+
+    const { data: tokens } = data.value
+    setTokens(tokens)
+
+    await fetchUser()
+
+    return currentUser
+  }
+
+  async function signout() {
+    await $useFetch('auth/signout')
+      .post({
+        refreshToken: localStorage.getItem('refreshToken') || ''
+      })
+      .json()
+
+    setUser(null)
+    localStorage.removeItem('accessToken')
+    localStorage.removeItem('refreshToken')
+  }
+
+  async function fetchUser() {
+    fetching.value = true
+    const fetchedUser = await getUserFromApi()
+    setUser(fetchedUser)
+    fetching.value = false
+    return currentUser.value
+  }
+
+  function setTokens({ accessToken, refreshToken }: SignAuthPayload) {
+    localStorage.setItem('accessToken', accessToken)
+    localStorage.setItem('refreshToken', refreshToken)
+  }
+
+  function clearTokens() {
+    localStorage.removeItem('accessToken')
+    localStorage.removeItem('refreshToken')
+  }
+
+  return {
+    currentUser,
+    isAuth,
+    isFetching: fetching,
+    setUser,
+    signout,
+    signIn,
+    signUp,
+    setTokens,
+    clearTokens,
+    fetchUser
+  }
+})
