c6141fc38058c9316fb9308d6316f2240fcc440d
Dinno <tahadlrb7@gmail.com>
2024-05-13 13:47:03 +0330

    feat(component): add main layout

---
+++ src/layouts/Main.vue
@@ -0,0 +1,15 @@
+<script setup type="ts">
+import Header from '../components/Header.vue';
+import Toast from '../components/Toast.vue';
+</script>
+
+<template>
+  <Header class="border-b-2" />
+  <main class="container px-10 mx-auto my-20">
+    <RouterView />
+  </main>
+  <footer>
+    <h1>This is footer</h1>
+  </footer>
+  <Toast />
+</template>
