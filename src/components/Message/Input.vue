e1fe2c8c29e7555628fa134818855df135c69231
Dinno <tahadlrb7@gmail.com>
2024-05-15 11:01:07 +0330

    feat(components): add Messages section components

---
+++ src/components/Message/Input.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Icon from '../Icon.vue';
+</script>
+
+<template>
+  <div class="h-14 min-h-12 flex items-center justify-center gap-3 rounded-lg">
+    <input
+      name="message"
+      id="message"
+      dir="auto"
+      class="h-full w-4/12 resize-none rounded-lg bg-brand-primary/10 px-3 py-1 backdrop-blur focus:(outline-none)"
+    />
+    <div
+      class="group flex cursor-pointer items-center justify-center rounded-lg bg-brand-primary/10 p-3"
+    >
+      <Icon
+        name="i-eva:navigation-2-outline"
+        color="text-brand-primary/70"
+        size="28"
+        class="group-hover:text-brand-action"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Message/View.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="messages rounded-lg bg-brand-primary/5 px-32 py-10">
+    <h1 class="bg-rose">Messages...</h1>
+  </div>
+</template>
+
+<style scoped></style>
