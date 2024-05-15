ddb4708510d3141a3ae3c8f5191f8502448b597f
Dinno <tahadlrb7@gmail.com>
2024-05-15 11:01:46 +0330

    feat(home page): add Logo to home page when not selected any user

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,23 +2,37 @@
 {
   name: 'home',
   meta: {
-    auth: true
-  }
+    auth: true,
+  },
 }
 </route>
 
 <script setup lang="ts">
-import SideBar from '@/components/SideBar.vue'
-import { useUser } from '@/composables/useUser'
+import Logo from '@/components/Logo.vue';
+import SideBar from '@/components/SideBar/Index.vue';
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
 
-const { currentUser, isAuth } = useUser()
+const route = useRoute();
+const isUserMessagePage = computed(() => (route.name as string)?.toLowerCase() === 'user_messages');
 </script>
 <template>
-  <div class="grid lg:grid-cols-[3fr_9fr] gap-5 h-screen max-h-screen">
-    <SideBar />
-    <section class="bg-sky">
+  <div class="grid h-screen max-h-screen gap-5 lg:grid-cols-[3fr_9fr]">
+    <section :class="[isUserMessagePage && '<lg:hidden']">
+      <SideBar />
+    </section>
+    <section :class="[!isUserMessagePage && '<lg:hidden']">
       <!-- > Refers to src/pages/index/[user_id].vue -->
       <RouterView />
+      <div v-if="!isUserMessagePage" class="size-full flex items-center justify-center">
+        <div class="flex items-center justify-center">
+          <Logo size="150" />
+          <span class="flex flex-col items-center justify-center gap-2">
+            <p class="text-5xl font-black font-hand">Dinno chat</p>
+            <p>Select user to send message</p>
+          </span>
+        </div>
+      </div>
     </section>
   </div>
 </template>
