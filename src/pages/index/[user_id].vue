ea2e53a7d6eb596590d90da1504a390008aef4a2
Dinno <tahadlrb7@gmail.com>
2024-05-15 11:02:03 +0330

    feat(page): user messages page

--- src/pages/index/[user_id].vue
+++ src/pages/index/[user_id].vue
@@ -1,8 +1,19 @@
-<script setup lang="ts"></script>
+<route lang="json5">
+{
+  name: 'user_messages',
+}
+</route>
+
+<script setup lang="ts">
+import MessageInput from '@/components/Message/Input.vue';
+import MessageView from '@/components/Message/View.vue';
+</script>
 
 <template>
-  <div>HIII</div>
-  <!-- {{ $route.params }} -->
+  <div class="h-full flex flex-col justify-between gap-5 px-3 py-5">
+    <MessageView class="flex-grow-1" />
+    <MessageInput />
+  </div>
 </template>
 
 <style scoped></style>
