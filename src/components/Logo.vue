75eb2ea8de58cd0f635a6738bbc962f8492b121e
Dinno <tahadlrb7@gmail.com>
2024-05-15 10:42:27 +0330

    refactor(component): logo.vue

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,22 +1,25 @@
 <script setup lang="ts">
-  interface IProps {
-    size?: number | string;
-  }
-  const props = defineProps<IProps>()
-
+interface IProps {
+  size?: number | string;
+  hasText?: boolean;
+}
+const props = withDefaults(defineProps<IProps>(), {
+  size: 74,
+  hasText: false,
+});
 </script>
 
 <template>
-  <div
-    :style="{ width: props.size + 'px' || '250px' }"
-    class="aspect-ratio"
-  >
-    <img
-      class="w-full h-full"
-      src="@/assets/logo.png"
-      alt="Dinno chat logo"
+  <div class="flex items-center justify-center">
+    <div :style="{ width: props.size + 'px' }" class="aspect-ratio">
+      <img class="h-full w-full" src="@/assets/logo.png" alt="Dinno chat logo" />
+    </div>
+    <p
+      v-if="hasText"
+      class="font-black font-hand"
+      :style="{ fontSize: parseFloat(props.size + '') / 3 + 'px' }"
     >
+      Dinno chat
+    </p>
   </div>
 </template>
-
-<style scoped></style>
