bd93038d13cdda23c4d3740de80c157c4d0d79be
Dinno <tahadlrb7@gmail.com>
2024-05-16 18:44:59 +0330

    feat(utils): add normalize image url function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,10 @@ export function generateRandUnique(len: number = 7): string {
     .toString(36)
     .substring(2, len + 2);
 }
+
+export function normalizeImageUrl(url: string = ''): string {
+  if (!url) return url;
+  return url.startsWith('http')
+    ? url
+    : `${import.meta.env.VITE_SERVER_URL}${import.meta.env.VITE_SERVER_URL.endsWith('/') || url.startsWith('/') ? '' : '/'}${url}`;
+}
