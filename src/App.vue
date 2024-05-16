10c1e14f5425df998ee9ad5b8952384b5c23a04f
Dinno <tahadlrb7@gmail.com>
2024-05-16 18:41:42 +0330

    feat(app.vue): check user Authorization in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
 import Toast from '@/components/Toast.vue';
+import { whenever } from '@vueuse/core';
+import { useRouter } from 'vue-router';
+import { useUser } from './composables/useUser';
+
+const { isAuth } = useUser();
+const router = useRouter();
+whenever(
+  () => !isAuth.value,
+  () => router.push({ name: 'signin', force: true, replace: true }),
+);
 </script>
 
 <template>
