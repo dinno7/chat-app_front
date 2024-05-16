7f4c46e90bf1359f60ea36c2b3604d97988695c1
Dinno <tahadlrb7@gmail.com>
2024-05-16 18:47:13 +0330

    feat(pinia state managment): add pinia to project

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
-import '@unocss/reset/tailwind-compat.css';
+import '@unocss/reset/tailwind.css';
 import 'uno.css';
 import './assets/main.css';
 
+import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
+const pinia = createPinia();
 const app = createApp(App);
 
+app.use(pinia);
 app.use(router);
 
 app.mount('#app');
