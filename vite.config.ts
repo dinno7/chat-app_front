4adf20d202c886c26a65ea9ad72df922954d0be9
Dinno <tahadlrb7@gmail.com>
2024-05-16 18:40:00 +0330

    feat(vite.config.ts): define .html aliases for routes

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,18 @@ import VueDevTools from 'vite-plugin-vue-devtools';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { port: 4000 },
-  plugins: [VueRouter(), Vue(), VueDevTools(), UnoCSS()],
+  plugins: [
+    VueRouter({
+      dts: 'src/typed-router.d.ts',
+      extendRoute(route) {
+        if (!['/:path(.*)', '/'].includes(route.path)) route.addAlias(`${route.path}.html`);
+        if (route.path === '/') route.addAlias(['/home', '/home.html', '/index', '/index.html']);
+      },
+    }),
+    Vue(),
+    VueDevTools(),
+    UnoCSS(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
