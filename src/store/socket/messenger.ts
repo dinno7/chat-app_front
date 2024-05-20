536df246042952171132c663dad4add66f5aa9cc
Dinno <tahadlrb7@gmail.com>
2024-05-20 20:21:59 +0330

    feat(messenger store): add all listeners

--- src/store/socket/messenger.ts
+++ src/store/socket/messenger.ts
@@ -1,18 +1,51 @@
 import { socket } from '@/socket';
+import type { Conversation, Message, User } from '@/types';
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
 export const useMessengerStore = defineStore('messenger', () => {
+  const messages = ref<Message[]>([]);
+  const receiverUser = ref<User | null>(null);
   const onlineUsers = ref<string[]>([]);
+  const conversations = ref<Conversation[]>([]);
+
   function clearOnlineUsers() {
     onlineUsers.value = [];
   }
 
+  const sendMessage = (text: string, imageUrl = null, videoUrl = null) => ({
+    to: (receiverId: string) =>
+      socket.send({
+        text,
+        to: receiverId,
+        videoUrl,
+        imageUrl,
+      }),
+  });
+
   function bindEvents() {
+    socket.on('message', (data) => {
+      messages.value = data;
+    });
+    socket.on('consumeReceiverUserDetails', (data) => {
+      receiverUser.value = data;
+    });
     socket.on('getOnlineUsers', (data) => {
       onlineUsers.value = JSON.parse(JSON.stringify(data));
     });
+
+    socket.on('getChatConversations', (data) => {
+      conversations.value = data;
+    });
   }
 
-  return { onlineUsers, clearOnlineUsers, bindEvents };
+  return {
+    onlineUsers,
+    messages,
+    receiverUser,
+    conversations,
+    clearOnlineUsers,
+    sendMessage,
+    bindEvents,
+  };
 });
