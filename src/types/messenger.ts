60581a121db686f024b91decf8c4f00337aa7517
Dinno <tahadlrb7@gmail.com>
2024-05-22 16:02:26 +0330

    chore(messenger types): add & modify some types

--- src/types/messenger.ts
+++ src/types/messenger.ts
@@ -2,18 +2,30 @@ import type { User } from './user';
 
 export interface Message {
   id: string;
-  from: string;
-  to: string;
   text: string;
   seen: boolean;
   imageUrl: string | null;
   videoUrl: string | null;
   createdAt: string | Date;
+  conversation: Conversation | string;
+  sender: User | string;
+  receiver: User | string;
 }
 
 export interface Conversation {
   id: string;
-  to: User;
+  user: User | string;
+  from: User | string;
+  to: User | string;
+  messages?: Message[];
   lastMessage: Message;
-  updatedAt: Date | string;
+  updatedAt: string;
+}
+
+export interface ConversationList {
+  id: string;
+  user: User;
+  lastMessage: Message;
+  updatedAt: string;
+  unseenMessagesCount: number;
 }
