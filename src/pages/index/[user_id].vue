bb8dde841ea55f8b07cb850fe32c7bd6585d3ec6
Dinno <tahadlrb7@gmail.com>
2024-05-22 16:08:35 +0330

    feat([user_id] page): complete this page(header, input, view, seen emit)

--- src/pages/index/[user_id].vue
+++ src/pages/index/[user_id].vue
@@ -5,15 +5,26 @@
 </route>
 
 <script setup lang="ts">
+import MessageHeader from '@/components/Message/Header.vue';
 import MessageInput from '@/components/Message/Input.vue';
 import MessageView from '@/components/Message/View.vue';
+import { socket } from '@/socket';
+import { watchEffect } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+watchEffect(() => {
+  socket.emit('provideReceiverUserDetails', route.params.user_id || '');
+  socket.emit('seen', route.params.user_id || '');
+});
 </script>
 
 <template>
-  <div class="h-full flex flex-col justify-between gap-5 px-3 py-5">
-    <MessageView class="flex-grow-1" />
-    <MessageInput />
+  <div class="h-full max-h-screen flex flex-col justify-between gap-2 px-3 py-5">
+    <MessageHeader class="basis-5rem" />
+    <MessageView ref="messages" class="flex-1" />
+    <MessageInput
+      class="sticky bottom-3 h-14 min-h-12 w-full flex basis-0.7/12 items-center justify-center"
+    />
   </div>
 </template>
-
-<style scoped></style>
